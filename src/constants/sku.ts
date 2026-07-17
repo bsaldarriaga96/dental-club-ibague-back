0947641cd221de9240176e877599cc159ca5c3f8
bsaldarriaga96 <b.saldamado@gmail.com>
2026-07-17 15:43:16 -0500

    Feat: Added new excluded sku

--- src/constants/sku.ts
+++ src/constants/sku.ts
@@ -1,1 +1,1 @@
-export const EXCLUDED_SKUS = ["000001", "1", "2", "01", "RegistroManual", "productogenericonube"] as const;
+export const EXCLUDED_SKUS = ["00000", "000001", "1", "2", "01", "RegistroManual", "productogenericonube"] as const;
